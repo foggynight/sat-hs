7d8c7f740920e07019f145e9c5998983899e6a82
Robert Coffey <robertcoffey709@gmail.com>
2026-07-03 15:45:08 -0230

    DP: replace main' with solve_DP

--- src/Main_DP.hs
+++ src/Main_DP.hs
@@ -190,58 +190,61 @@ varOrderJeroslowWang (CNF n_vars _ clauses) =
           sum $ map (\c -> 2 ^^ (- (length c)) :: Double)
                 $ filter (clauseHasVar var) clauses
 
-main' :: DIMACS_CNF -> VarOrderF -> IO ()
-main' dimacs_cnf vo_func = do
-  let maybe_cnf = parseDIMACS dimacs_cnf
-  case maybe_cnf of
-    Nothing  -> putStrLn "error: invalid CNF"
-    Just cnf -> do
-      putStr "Initial CNF: "
-      print cnf
-      newline
+solve_DP :: CNF -> VarOrderF -> IO ()
+solve_DP cnf vo_func = do
+  putStr "Initial CNF: "
+  print cnf
+  newline
 
-      let var_order = vo_func cnf
-      when (last var_order /= 0) $ do
-        error "error: variable order doesn't end with 0"
-      putStr "Variable Order: "
-      print var_order
-      newline
+  let var_order = vo_func cnf
+  when (last var_order /= 0) $ do
+    error "error: variable order doesn't end with 0"
+  putStr "Variable Order: "
+  print var_order
+  newline
 
-      let init_buks = fillBuckets var_order (cnf_clauses cnf)
-      putStrLn "Initial Buckets: "
-      mapM_ print init_buks
-      newline
+  let init_buks = fillBuckets var_order (cnf_clauses cnf)
+  putStrLn "Initial Buckets: "
+  mapM_ print init_buks
+  newline
 
-      putStrLn "Deriving resolvents..."
-      res_buks <- resolveBuckets init_buks
-      let res_cnt =
-            (sum $ map (length . buk_clauses) res_buks)
-            - (cnf_n_clauses cnf)
-      putStr $ "Resolvents added to buckets: " ++ show res_cnt
-      newline; newline
+  putStrLn "Deriving resolvents..."
+  res_buks <- resolveBuckets init_buks
+  let res_cnt =
+        (sum $ map (length . buk_clauses) res_buks)
+        - (cnf_n_clauses cnf)
+  putStr $ "Resolvents added to buckets: " ++ show res_cnt
+  newline; newline
 
-      putStrLn "Resolved Buckets: "
-      mapM_ print res_buks
-      newline
+  putStrLn "Resolved Buckets: "
+  mapM_ print res_buks
+  newline
 
-      let solution = extractSolution res_buks
-      case solution of
-        Nothing   -> putStrLn "UNSAT"
-        Just lits -> putStr $ "SAT: " ++ (show lits) ++ "\n"
+  let solution = extractSolution res_buks
+  case solution of
+    Nothing   -> putStrLn "UNSAT"
+    Just lits -> putStr $ "SAT: " ++ (show lits) ++ "\n"
 
 main :: IO ()
 main = do
   config <- execParser opts
+
   let var_order = case map toLower (conf_var_order config) of
                     "numeric" -> varOrderNumeric
                     "fewest"  -> varOrderFewestClauses
                     "jw"      -> varOrderJeroslowWang
                     _         -> trace "error: invalid variable order argument"
                                        varOrderNumeric
+
   dimacs_cnf <- case conf_cnf_file config of
                   "--" -> getContents
                   file -> readFile file
-  main' dimacs_cnf var_order
+
+  let maybe_cnf = parseDIMACS dimacs_cnf
+  case maybe_cnf of
+    Nothing  -> putStrLn "error: invalid CNF"
+    Just cnf -> do solve_DP cnf var_order
+
   where opts = info (configParser <**> helper)
                ( fullDesc
                  <> header ( "SatDP - SAT solver using DP algorithm implemented"
